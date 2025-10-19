042f35b51a2cdef4c86618b1ba2b3d68ec17ead7
Dobbymin <gmin990624@gmail.com>
2025-10-19 17:20:35 +0900

    feat: 구분자상수화 및 매직넘버 구현 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,11 @@
 import { ERROR_MESSAGE } from './message/error-message.js';
 import { PRINT_MESSAGE } from './message/print-message.js';
+import { SEPARATOR } from './separator/separator.js';
+import { NUMBER } from './number/number.js';
 
 export const CONSTANTS = Object.freeze({
   ...ERROR_MESSAGE,
   ...PRINT_MESSAGE,
+  ...SEPARATOR,
+  ...NUMBER,
 });
